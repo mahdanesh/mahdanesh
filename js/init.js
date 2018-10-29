63a2772db04ca24fdedcc1e9835be9c45468674f
Mahsa <danesh.mah9@gmail.com>
2018-10-29 16:23:12 +0330

    add

--- js/init.js
+++ js/init.js
@@ -5,3 +5,26 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems);
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+});
+
+window.sr = ScrollReveal();
+
+sr.reveal('.main-anim', {
+     duration: 1400,
+     origin: 'left',
+     delay: 300,
+});
+
+sr.reveal('.resume-anim', {
+     duration: 1400,
+     origin: 'left',
+     viewFactor: 0.5,
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.collapsible');
+var instances = M.Collapsible.init(elems, options);
+});
